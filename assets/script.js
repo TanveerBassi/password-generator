2af625d287e7a58cf69aabf436c44f6abadbc7fe
Tanveer Bassi <tanveer13a551@gmail.com>
2022-04-21 15:14:18 +0100

    updated getpassword criteria with prompts and array

--- assets/script.js
+++ assets/script.js
@@ -10,13 +10,46 @@ const getPasswordLength = () => {
   const passwordLength = parseInt(userInput);
 
   if (!isNaN(passwordLength) && passwordLength >= 8 && passwordLength <= 128) {
-    // If passwordlenght isn't a number or less then 8 or above 128 characters - return password length
+    // If password length isn't a number or less then 8 or above 128 characters - return password length
     return passwordLength;
   }
   //if statement criteria is not met prompt alert to put in valid section
   else {
     alert("Please put in a valid selection");
+    window.location.reload(true);
+    document.getElementById("password").reset();
   }
+};
+
+const getPasswordCriteria = () => {
+  // four prompts created - which are variables
+  const lowercase = confirm("do you want lowercase in your password?");
+  const uppercase = confirm("do you want uppercase in your password?");
+  const numeric = confirm("do you want numbers in your password?");
+  const specialCharacter = confirm(
+    "do you want special characters in your password?"
+  );
+
+  //empty array created to hold user choice
+  const choiceArray = [];
+  // if one user choices one of the selected prompts push into array
+  if (lowercase) {
+    choiceArray.push("abcdefghijklmnopqrstuvwxyz");
+  }
+
+  if (uppercase) {
+    choiceArray.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  }
+
+  if (numeric) {
+    choiceArray.push("0123456789");
+  }
+
+  if (specialCharacter) {
+    choiceArray.push("%&'()*+,-./:;<=>?@[]^_`");
+  }
+
+  return choiceArray;
 };
 
 // main function to generate the random password
